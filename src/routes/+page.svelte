650128630432a683d1421e2e60564b5f9b76948c
Denis <iamdenisgradinaru@gmail.com>
2026-03-23 18:57:35 +0200

    add mobile unavailable screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
-<script>
+<script lang="ts">
 	import Editor from './Editor.svelte';
 	import LoadingPage from '@views/LoadingPage.svelte';
+	import MobileUnavailable from '@views/MobileUnavailable.svelte';
+
+	const isMobile = globalThis.window
+		? window.matchMedia('(max-width: 768px)').matches
+		: false;
 </script>
 
 <svelte:head>
 	<title>ASCII Editor</title>
 </svelte:head>
 
-<LoadingPage />
-<Editor />
+<MobileUnavailable />
+
+{#if !isMobile}
+	<LoadingPage />
+	<Editor />
+{/if}
